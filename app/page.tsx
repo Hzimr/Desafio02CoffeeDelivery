5c2abadce205f21819c262214b38691f76c65d4c
Hítalo Rodrigues <hitalo005@gmail.com>
2024-11-20 22:39:03 -0300

    feat: create cart button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import { FaShoppingCart } from "react-icons/fa";
+
 export default function Home() {
-  return <>oi</>;
+  return (
+    <>
+      <header className="flex justify-center">
+        <nav className="bg-purple flex w-full max-w-[1440px] justify-between px-40 py-8">
+          <div>Hello</div>
+          <section className="flex flex-row gap-3">
+            <p>local</p>
+            <button className="text-yellowDark bg-yellowLight h-10 w-10 rounded-lg p-2">
+              <FaShoppingCart size={22} />
+            </button>
+          </section>
+        </nav>
+      </header>
+      <p>Main</p>
+    </>
+  );
 }
